225700bd77599c815db308403c663e62425e17dc
hieplai <hieplqph50087@gmail.com>
2024-04-29 09:17:24 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+  titleWeb: "Ngày mới vuii vẻ ^^",
+  introTitle: "Em bé hay cười",
+  introDesc: `Ngày mới vuii vẻ cậu nháaa`,
+  btnIntro: "^^HiHi^^",
+  title: "Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰",
+  desc: "Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ",
+  btnYes: "Thích lắm <33",
+  btnNo: "Không nha :3",
+  question: "Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3",
+  btnReply: "Gửi cho bạn <3",
+  reply: "Yêu thì yêu mà không yêu thì yêu <33333333",
+  mess: "Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘",
+  messDesc: "Nayyy call cho mình nhóa!",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://hieplai.github.io/to_tinh/", //gắn link facebook vào đây
+};
